2bb8f336be648ce90942cc5502dc0e4530e71e6e
reem-walid5 <reemw253@gmail.com>
2026-04-14 18:38:40 +0200

    fix: nextauth authorize return null

--- src/app/Nextauth/NextAuth.config.ts
+++ src/app/Nextauth/NextAuth.config.ts
@@ -1,6 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { jwtDecode } from 'jwt-decode';
+import { jwtDecode } from "jwt-decode";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -12,6 +12,7 @@ export const authOptions: NextAuthOptions = {
       },
       async authorize(credentials) {
         console.log("CREDENTIALS:", credentials);
+
         try {
           const res = await fetch(
             "https://ecommerce.routemisr.com/api/v1/auth/signin",
@@ -19,42 +20,47 @@ export const authOptions: NextAuthOptions = {
               method: "POST",
               body: JSON.stringify(credentials),
               headers: { "Content-Type": "application/json" },
-            },
+            }
           );
 
           const finalRes = await res.json();
           console.log("finalRes", finalRes);
+
           if (res.ok && finalRes.token && finalRes.user) {
-            const data:{id : string} = jwtDecode(finalRes.token)
+            const data: { id: string } = jwtDecode(finalRes.token);
+
             return {
-              id:data.id,
+              id: data.id,
               name: finalRes.user.name,
               email: finalRes.user.email,
-              credentialsToken : finalRes.token,
+              credentialsToken: finalRes.token,
             };
           }
+          return null;
+
         } catch (error) {
           console.log("AUTH ERROR:", error);
-          throw new Error("Authentication failed"); 
+          return null;
         }
       },
     }),
   ],
-  // jwt: { maxAge: 60 * 60 * 24 * 3 },
-  pages: { signIn: "/LogIn" },
-  callbacks: {
-  jwt({ token, user }) {
-    if (user) {
-      token.routeToken = (user as any).credentialsToken;
-      token.id = user.id;
-    }
-    return token;
-  },
 
-  session({ session, token }) {
-    (session as any).routeToken = token.routeToken;
-    (session as any).id = token.id;
-    return session;
+  pages: { signIn: "/LogIn" },
+
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.routeToken = (user as any).credentialsToken;
+        token.id = user.id;
+      }
+      return token;
+    },
+
+    session({ session, token }) {
+      (session as any).routeToken = token.routeToken;
+      (session as any).id = token.id;
+      return session;
+    },
   },
-}
 };
