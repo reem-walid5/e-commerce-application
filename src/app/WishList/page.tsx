7c729b2312f6f878c23947d42875970fd7ecb124
reem-walid5 <reemw253@gmail.com>
2026-04-14 18:44:35 +0200

    fix: update wishlist

--- src/app/WishList/page.tsx
+++ src/app/WishList/page.tsx
@@ -8,17 +8,43 @@ import DeleteProductFromWishlist from "./DeleteProductFromWishlist";
 
 export default async function page() {
   const userWishlist = await getWishlistProducts();
-  //   if (userWishlist === undefined) {
-  //     return new Error("session ended , please login again");
-  //   }
-  if (!userWishlist) {
-    return;
+
+  if (userWishlist === undefined) {
+    return (
+      <div className="text-center mt-10 text-red-500">
+        session ended , please login again
+      </div>
+    );
   }
-  // totalCartPrice => cartResponse
+
+  if (!userWishlist || userWishlist.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen gap-5">
+        <div className="bg-red-100 p-5 rounded-full">
+          <FaHeart className="text-red-500 text-3xl" />
+        </div>
+
+        <h2 className="text-2xl font-semibold text-gray-700">
+          Your wishlist is empty 
+        </h2>
+
+        <p className="text-gray-500">
+          Start adding products you love
+        </p>
+
+        <Link href="/">
+          <button className="bg-green-600 text-white px-6 py-2 rounded-2xl hover:bg-green-700 transition">
+            Go to shopping
+          </button>
+        </Link>
+      </div>
+    );
+  }
+
   const products = userWishlist;
+
   return (
     <div className="min-h-screen">
-      {/* Header */}
       <div className="bg-gray-50 py-12 mb-12">
         <div className="flex items-center gap-4 container m-auto">
           <div className="bg-red-100 p-4 rounded-xl">
@@ -37,7 +63,6 @@ export default async function page() {
       {/* Table */}
       <div className=" container m-auto">
         <Card className="rounded-2xl p-0 overflow-hidden mb-10">
-          {/* Head */}
           <div className="grid grid-cols-4 px-6 py-4 text-gray-500 text-[16px] font-semibold border-b">
             <p className="text-left ml-20">Product</p>
             <p className="text-center">Price</p>
@@ -45,7 +70,7 @@ export default async function page() {
             <p className="text-center ml-20">Actions</p>
           </div>
 
-          {products?.map((product) => (
+          {products.map((product) => (
             <div
               className="grid grid-cols-4 items-center px-6 py-5 border-b"
               key={product._id}
@@ -110,7 +135,6 @@ export default async function page() {
           ))}
         </Card>
 
-        {/* Continue Shopping */}
         <Link
           href="/"
           className=" text-gray-600 text-[18px] cursor-pointer hover:underline hover:text-green-500 transition-all duration-150"
