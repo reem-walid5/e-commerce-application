3119cbe939bf81e698e94b9e1fa1e2c07d4c44e1
reem-walid5 <reemw253@gmail.com>
2026-04-15 22:43:06 +0200

    fix auth and cookies issue

--- src/app/Utilis.ts
+++ src/app/Utilis.ts
@@ -1,14 +1,36 @@
+// import { decode } from "next-auth/jwt";
+// import { cookies } from "next/headers";
+
+// export default async function getUserToken(): Promise<string| null> {
+//     const cookie =await cookies()
+//     const NextAuthToken = cookie.get('next-auth.session-token')?.value;
+//     const jwtRes= await decode({secret:process.env.NEXTAUTH_SECRET || "" , token:NextAuthToken})
+//     if (jwtRes) {
+//         return jwtRes.routeToken as string
+//     }else{
+//         return null
+//     }
+
+// }
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-export default async function getUserToken(): Promise<string| null> {
-    const cookie =await cookies()
-    const NextAuthToken = cookie.get('next-auth.session-token')?.value;
-    const jwtRes= await decode({secret:process.env.NEXTAUTH_SECRET || "" , token:NextAuthToken})
-    if (jwtRes) {
-        return jwtRes.routeToken as string
-    }else{
-        return null
-    }
+export default async function getUserToken(): Promise<string | null> {
+  const cookieStore = cookies();
+  const NextAuthToken =
+    cookieStore.get("next-auth.session-token")?.value ||
+    cookieStore.get("__Secure-next-auth.session-token")?.value;
 
+  if (!NextAuthToken) return null;
+
+  const jwtRes = await decode({
+    secret: process.env.NEXTAUTH_SECRET || "",
+    token: NextAuthToken,
+  });
+
+  if (jwtRes) {
+    return jwtRes.routeToken as string;
+  }
+
+  return null;
 }
